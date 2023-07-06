76d6897b588350724addc20478d8f5e79bc880c5
huyshogun <huy15a3k23@cvp.vn>
2023-07-07 00:30:25 +0700

    init project

---
+++ helloworld.php
@@ -0,0 +1,18 @@
+<div class ="content">
+    <?php
+    echo "Hello PHP";
+    echo "<br>Hello world";
+    ?>
+
+<!--</div>-->
+<!--<style type="text/css ">-->
+<!--    * {-->
+<!--        margin: 0px;-->
+<!--        padding: 0px;-->
+<!--    }-->
+<!--    .content {-->
+<!--        width: 600px;-->
+<!--        height: 300px;-->
+<!--        background: #7d0000;-->
+<!--    }-->
+<!--</style>-->
